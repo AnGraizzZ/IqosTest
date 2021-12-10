ffcf3119e75677fc7d57031ae530f0c7bb749b3d
AnGraizzZ <pekpak@mail.ru>
2021-12-10 12:48:56 +0300

    WP1

--- header.php
+++ header.php
@@ -1,59 +1,24 @@
-<?php
-/**
- * The header for our theme
- *
- * This is the template that displays all of the <head> section and everything up until <div id="content">
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package iqostest
- */
-
-?>
-<!doctype html>
-<html <?php language_attributes(); ?>>
+<!DOCTYPE html>
+<html lang="ru">
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php bloginfo( 'template_directory' )?> /css/stile.css">
+    <title>iqos</title>
 
-	<?php wp_head(); ?>
 </head>
-
-<body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'iqostest' ); ?></a>
-
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$iqostest_description = get_bloginfo( 'description', 'display' );
-			if ( $iqostest_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $iqostest_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'iqostest' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+<body>
+    <header>
+        <div class="Top">
+            <div class="BackTop">
+               <img src="<?php bloginfo( 'template_directory' )?> /image/Frame.png" alt="111">
+               <div class="infoTop"><p>БАЛЛЫ НА ФИНИШНОЙ ПРЯМОЙ</p></div>
+               <div class="infoBonusTop"><p>Тратьте ваши баллы IQOS CLUB на высокие цели</p></div>
+               <div class="infoBonusTop infoDownTop" ><p>Узнать больше</p>
+                <br>
+                <embed src="<?php bloginfo( 'template_directory' )?> /image/Rectangle 322.svg" width="80px"></embed>
+            </div>
+            </div>
+        </div>
+    </header>
